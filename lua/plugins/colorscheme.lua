e4c7dd88aa917966bfcb4595541352ed05a96d91
marhiru <marcosgamern@gmail.com>
2025-10-17 23:04:09 -0300

    feat: added new colorschemes

--- lua/plugins/colorscheme.lua
+++ lua/plugins/colorscheme.lua
@@ -15,22 +15,58 @@ return {
         end,
     },
     {
+        "vague-theme/vague.nvim",
+        lazy = false,
+        name = "vague",
+        priority = 1000,
+        config = function()
+            require("vague").setup({})
+        end,
+    },
+
+    {
+        "dgox16/oldworld.nvim",
+        lazy = false,
+        priority = 1000,
+        config = function()
+            vim.cmd([[colorscheme oldworld]])
+        end,
+    },
+    -- {
+    --     "chrsm/paramount-ng.nvim",
+    --     dependencies = {
+    --         "rktjmp/lush.nvim",
+    --     },
+    --     priority = 1000,
+    --     config = function()
+    --         vim.cmd([[colorscheme paramount-ng]])
+    --         vim.api.nvim_set_hl(0, "Normal", { bg = "#0f0f17" })
+    --         vim.api.nvim_set_hl(0, "NormalFlo;at", { bg = "#0f0f17" })
+    --         vim.api.nvim_set_hl(0, "NormalNC", { bg = "#0f0f17" })
+    --     end,
+    -- },
+    {
         "metalelf0/black-metal-theme-neovim",
         lazy = false,
         name = "black-metal",
         config = function()
             require("black-metal").setup({
-                theme = "taake",
+                theme = "khold",
                 variant = "dark",
                 alt_bg = false,
             })
-            require("black-metal").load()
+            -- require("black-metal").load()
         end,
     },
     {
-        "LazyVim/LazyVim",
-        opts = {
-            colorscheme = "black-metal",
-        },
+        "ellisonleao/gruvbox.nvim",
+        config = true,
+        opts = {},
     },
+    -- {
+    --     "LazyVim/LazyVim",
+    --     opts = {
+    --         colorscheme = "oh-lucy",
+    --     },
+    -- },
 }
