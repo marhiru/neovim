ce541cacd2a6f36a947ef7e1ef9d54f6ccd49ed9
marhiru <marcosgamern@gmail.com>
2025-10-21 17:23:50 -0300

    fix: disabled snacks option: scroll

---
+++ lua/plugins/snacks.lua
@@ -0,0 +1,18 @@
+return {
+    "folke/snacks.nvim",
+    opts = {
+        picker = {
+            sources = {
+                files = {
+                    hidden = true,
+                },
+            },
+        },
+        explorer = {
+            files = {
+                hidden = true,
+            },
+        },
+        scroll = { enabled = false }
+    },
+}
