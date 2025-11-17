0f3429c908565612bb9dcf543405ec3800e24a81
marhiru <marcosgamern@gmail.com>
2025-11-17 20:11:29 -0300

    fix: removed xeno as main theme

--- lua/plugins/xeno.lua
+++ lua/plugins/xeno.lua
@@ -1,12 +1,12 @@
 return {
-  'kyza0d/xeno.nvim',
-  lazy = false,
-  priority = 1000,
-  config = function()
-    require('xeno').new_theme('xeno-pink-haze', {
-      base = '#0f0c0e',
-      accent = '#D19EBC',
-      contrast = 0.3,
-    })
-  end,
+    'kyza0d/xeno.nvim',
+    lazy = false,
+    priority = 1000,
+    config = function()
+        require('xeno').new_theme('xeno-pink-haze', {
+            base = '#0f0c0e',
+            accent = '#D19EBC',
+            contrast = 0.1,
+        })
+    end,
 }
