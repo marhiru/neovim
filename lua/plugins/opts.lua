a9b9b7f65c79d467067bcb84575b94c4dee26a52
marhiru <marcosgamern@gmail.com>
2025-11-19 17:44:12 -0300

    fix: removed mason-lspconfig as disabled

--- lua/plugins/opts.lua
+++ lua/plugins/opts.lua
@@ -1,4 +1,5 @@
 return {
     { "stevearc/conform.nvim", enabled = false },
-    { "mason-lspconfig.nvim",  enabled = true },
+    -- { "mason-lspconfig.nvim",  enabled = false },
+    -- { "mason.nvim",            enabled = false },
 }
