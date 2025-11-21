2cc20a9d6e68df19cdb4641576d327ac11ab0a46
marhiru <marcosgamern@gmail.com>
2025-11-21 17:20:29 -0300

    feat: improved neo-tree config

--- lua/plugins/neo-tree.lua
+++ lua/plugins/neo-tree.lua
@@ -1,6 +1,27 @@
 return {
     "nvim-neo-tree/neo-tree.nvim",
-    opts = {},
+    opts = {
+        close_if_last_window = true,
+        popup_border_style = "rounded",
+        enable_git_status = true,
+        enable_diagnostics = true,
+        filesystem = {
+            follow_current_file = { enabled = true },
+            hijack_netrw_behavior = "open_current",
+            use_libuv_file_watcher = true,
+        },
+        window = {
+            position = "current",
+            width = 35,
+        },
+        default_component_configs = {
+            indent = {
+                with_expanders = true,
+                expander_collapsed = "",
+                expander_expanded = "",
+            },
+        },
+    },
     branch = "v3.x",
     dependencies = {
         "nvim-lua/plenary.nvim",
@@ -8,21 +29,11 @@ return {
         "nvim-tree/nvim-web-devicons",
     },
     keys = {
-        {
-            "<leader>e",
-            function()
-                pcall(require("neo-tree").setup({}))
-            end,
-            mode = { "n", "x" },
-            desc = "Expĺorer (current dir)"
-        }
+        { "<leader>e", "<cmd>Neotree toggle reveal position=current<CR>", desc = "Explorer (current window)" },
+        { "<leader>E", "<cmd>Neotree reveal position=current<CR>",        desc = "Explorer Reveal (current window)" },
     },
     lazy = false,
     config = function()
-        require("neo-tree").setup({
-            window = {
-                position = "current"
-            }
-        })
+        require("neo-tree").setup({})
     end
 }
