87fd6ebe53d97467d6f874db3209c33acb84bfae
marhiru <marcosgamern@gmail.com>
2025-12-13 21:56:49 -0300

    fix: removed inlay_hint enabled

--- init.lua
+++ init.lua
@@ -4,7 +4,6 @@ require("config.lazy")
 vim.opt.shiftwidth = 4
 vim.o.guifont = "JetBrainsMono Nerd Font:h14:w8"
 vim.opt.clipboard = "unnamedplus"
-vim.lsp.inlay_hint.enable(false, nil)
 vim.cmd("TransparencyToggle")
 
 ------------
