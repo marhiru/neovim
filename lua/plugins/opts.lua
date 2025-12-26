da21595150d0abda371a2db85146d945f3966cb5
marhiru <marcosgamern@gmail.com>
2025-12-25 22:21:51 -0300

    fix: disabled plugin

--- lua/plugins/opts.lua
+++ lua/plugins/opts.lua
@@ -1,5 +1,6 @@
 return {
-    { "stevearc/conform.nvim",           enabled = false },
-    { "mason-org/mason-lspconfig.nvim",  enabled = false },
-    { "nvim-telescope/telescope.nvim",   enabled = false },
+    { "stevearc/conform.nvim",          enabled = false },
+    { "mason-org/mason-lspconfig.nvim", enabled = false },
+    { "nvim-telescope/telescope.nvim",  enabled = false },
+    { "saghen/blink.cmp",               enabled = false }
 }
