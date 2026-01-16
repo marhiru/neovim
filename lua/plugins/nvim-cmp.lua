4be4e487d644590a2fb1f3477fd1e7a656552934
marhiru <marcosgamern@gmail.com>
2026-01-16 14:22:17 -0300

    feat: added nvim-cmp completion plugin

---
+++ lua/plugins/nvim-cmp.lua
@@ -0,0 +1,18 @@
+return {
+    {
+        "hrsh7th/nvim-cmp",
+        lazy = false,
+        priority = 1000,
+        dependencies = {
+            "onsails/lspkind.nvim",
+            "hrsh7th/cmp-nvim-lsp",
+            "hrsh7th/cmp-path",
+            "hrsh7th/cmp-buffer",
+            { "L3M0N4D3/LuaSnip", build = "make install_jsregexp" },
+            "saadparwaiz1/cmp_luasnip",
+        },
+        config = function()
+            require "custom.completion"
+        end
+    }
+}
