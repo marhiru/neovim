9e796b9e29b3ae5880c3684fbb18a87a33252463
marhiru <marcosgamern@gmail.com>
2026-01-16 21:55:21 -0300

    fix: fixed value

--- lua/plugins/opts.lua
+++ lua/plugins/opts.lua
@@ -2,5 +2,5 @@ return {
     { "stevearc/conform.nvim",          enabled = false },
     { "mason-org/mason-lspconfig.nvim", enabled = false },
     { "nvim-telescope/telescope.nvim",  enabled = false },
-    { "saghen/blink.cmp",               enabled = true }
+    { "saghen/blink.cmp",               enabled = false }
 }
