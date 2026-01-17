5fc33700fc47a54174ac0d7a9b58e2ad6f43867b
marhiru <marcosgamern@gmail.com>
2026-01-16 21:53:14 -0300

    fix; fixed packages installation

--- lua/plugins/nvim-cmp.lua
+++ lua/plugins/nvim-cmp.lua
@@ -8,11 +8,12 @@ return {
             "hrsh7th/cmp-nvim-lsp",
             "hrsh7th/cmp-path",
             "hrsh7th/cmp-buffer",
-            { "L3M0N4D3/LuaSnip", build = "make install_jsregexp" },
+            { "L3MON4D3/LuaSnip", build = "make install_jsregexp" },
             "saadparwaiz1/cmp_luasnip",
         },
         config = function()
-            require "custom.completion"
+            require "config.completion"
+            require("nvim-cmp").setup({})
         end
     }
 }
