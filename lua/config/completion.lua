6e81eb025b890877280933357d2a9891019896fc
marhiru <marcosgamern@gmail.com>
2026-01-16 23:08:00 -0300

    fix: changed confirm keybind

--- lua/config/completion.lua
+++ lua/config/completion.lua
@@ -17,7 +17,7 @@ cmp.setup {
     mapping = {
         ["<C-n>"] = cmp.mapping.select_next_item { behavior = cmp.SelectBehavior.Insert },
         ["<C-p>"] = cmp.mapping.select_prev_item { behavior = cmp.SelectBehavior.Insert },
-        ["<C-Space>"] = cmp.mapping(
+        ["<C-m>"] = cmp.mapping(
             cmp.mapping.confirm {
                 behavior = cmp.ConfirmBehavior.Insert,
                 select = true,
