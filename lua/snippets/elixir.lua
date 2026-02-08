e039b8b684e62e01c83c0fad7f8523719142f393
marhiru <marcosgamern@gmail.com>
2026-02-08 15:06:28 -0300

    feat: added new elixir snippet

---
+++ lua/snippets/elixir.lua
@@ -0,0 +1,17 @@
+local Snippets
+require("snippets.config")
+
+local m = Snippets.mode
+
+Snippets.new("elixir", {
+    Snippets.name("el",
+        Snippets.fmt("<%= if {} do %>{}<% end %>{}", {
+            m(1), m(2), m(0)
+        })
+    ),
+    Snippets.name("el",
+        Snippets.fmt("<%= {} %>{}", {
+            m(1), m(0)
+        })
+    )
+})
