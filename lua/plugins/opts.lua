f1d46aed38b2c67166caa28a2b62e50c5a9c2e6a
marhiru <marcosgamern@gmail.com>
2026-02-24 14:10:36 -0300

    fix: removed opts files

--- lua/plugins/opts.lua
+++
@@ -1,7 +0,0 @@
-return {
-    { "stevearc/conform.nvim",          enabled = false },
-    { "mason-org/mason-lspconfig.nvim", enabled = true },
-    { "mason-org/mason.nvim",           enabled = true },
-    { "nvim-telescope/telescope.nvim",  enabled = false },
-    { "saghen/blink.cmp",               enabled = false }
-}
