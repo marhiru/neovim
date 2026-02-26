c9ab198a7d45f9449e51f33b08346e8efcd5960a
marhiru <marcosgamern@gmail.com>
2026-02-26 11:54:47 -0300

    fiX: formatted correctly (test format)

--- init.lua
+++ init.lua
@@ -15,21 +15,21 @@ require("config.autocmds")
 
 -- Set colorscheme (find theme with used = true)
 local function load_used_colorscheme()
-    local ok, spec = pcall(require, "plugins.colorscheme")
-    if not ok then
-        vim.cmd.colorscheme("oldtale")
-        return
-    end
+local ok, spec = pcall(require, "plugins.colorscheme")
+	if not ok then
+		vim.cmd.colorscheme("oldtale")
+		return
+	end
 
-    local theme_name = "oldtale" -- default
-    for _, theme in ipairs(spec) do
-        if theme.used then
-            theme_name = theme.name or theme[1]:match("/([^/]+)$")
-            break
-        end
-    end
+	local theme_name = "oldtale" -- default
+	for _, theme in ipairs(spec) do
+		if theme.used then
+			theme_name = theme.name or theme[1]:match("/([^/]+)$")
+			break
+		end
+	end
 
-    vim.cmd.colorscheme(theme_name)
+	vim.cmd.colorscheme(theme_name)
 end
 
 load_used_colorscheme()
