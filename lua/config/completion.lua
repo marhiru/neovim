be5823679aeaf219b8098276176bed6e24042480
marhiru <marcosgamern@gmail.com>
2026-02-26 11:15:04 -0300

    fix: fixed completion

--- lua/config/completion.lua
+++ lua/config/completion.lua
@@ -1,6 +1,8 @@
 vim.opt.completeopt = { "menu", "menuone", "noselect" }
 vim.opt.shortmess:append("c")
 
+local ls = require("luasnip")
+
 local cmp_nvim_lsp = require("cmp_nvim_lsp")
 vim.lsp.config("*", { capabilities = cmp_nvim_lsp.default_capabilities() })
 
@@ -19,6 +21,7 @@ cmp.setup({
     },
     sources = cmp.config.sources({
         { name = "nvim_lsp", priority = 1000 },
+        { name = "lazydev", group_index = 0 },
         { name = "luasnip", priority = 900 },
         { name = "path", priority = 700 },
     }, {
@@ -33,8 +36,8 @@ cmp.setup({
         ["<Tab>"] = cmp.mapping(function(fallback)
             if cmp.visible() then
                 cmp.select_next_item()
-            elseif require("luasnip").expand_or_jumpable() then
-                require("luasnip").expand_or_jump()
+            elseif ls.expand_or_jumpable() then
+                ls.expand_or_jump()
             else
                 fallback()
             end
@@ -42,8 +45,8 @@ cmp.setup({
         ["<S-Tab>"] = cmp.mapping(function(fallback)
             if cmp.visible() then
                 cmp.select_prev_item()
-            elseif require("luasnip").jumpable(-1) then
-                require("luasnip").jump(-1)
+            elseif ls.jumpable(-1) then
+                ls.jump(-1)
             else
                 fallback()
             end
@@ -51,7 +54,7 @@ cmp.setup({
     }),
     snippet = {
         expand = function(args)
-            require("luasnip").lsp_expand(args.body)
+            ls.lsp_expand(args.body)
         end,
     },
     formatting = {
@@ -67,7 +70,6 @@ cmp.setup({
     },
 })
 
-local ls = require("luasnip")
 ls.config.set_config({
     history = false,
     updateevents = "TextChanged,TextChangedI",
