a9e786274fdb2afbdd17352e819866127bef718d
marhiru <marcosgamern@gmail.com>
2026-02-26 13:56:01 -0300

    fix: formatted

--- lua/plugins/terminal.lua
+++ lua/plugins/terminal.lua
@@ -1,12 +1,12 @@
 local set = vim.opt_local
 
 vim.api.nvim_create_autocmd("TermOpen", {
-	group = vim.api.nvim_create_autocmd("custom-term-open", {}),
-	callback = function()
-	    set.number = false
-	    set.relativenumber = false
-	    set.scrolloff = 0
+    group = vim.api.nvim_create_autocmd("custom-term-open", {}),
+    callback = function()
+        set.number = false
+        set.relativenumber = false
+        set.scrolloff = 0
 
-	    vim.bo.filetype = "terminal"
-	end,
+        vim.bo.filetype = "terminal"
+    end,
 })
